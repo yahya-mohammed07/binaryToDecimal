b6ae1d239d27d172cda6b34c13a414819112ad46
yahya-mohammed07 <goldroger300@gmail.com>
2020-08-13 18:16:43 +0300

    new

--- main.cpp
+++ main.cpp
@@ -4,38 +4,37 @@
 #include <algorithm>
 #include <iomanip>
 //*
-int rBinToDec(unsigned long & real) //* takes the real numbers form the binary
+int rBinToDec(const std::string & integer) //* takes the real numbers form the binary
 {
-    long i = 0;
-    unsigned long result = 0;
-    while(real != 0)
+    int j = 0;
+    int result = 0;
+    for (int i = integer.length()-1; i >= 0; i--, j++)
     {
-        result += (real % 10) * std::pow(2,i);
-        if(real % 10 > 1 || real % 10 < 0)  //* to check if the number is bin or not
-        {
-            throw std::domain_error("not binary system!"); //* throw run-time error 
-                                                           //*if the the number is not binary
-        }
-        real  = real / 10;
-        i++;
-    }
-    return result;
-}
-//*
-float fracBinToDec(unsigned long & frac) //* takes the fraction from the bin number
-{
-    long i = -1;
-    float result = 0;
-    while(frac != 0)
-    {
-        result += (frac % 10) * std::pow(2,i);
-        if(frac % 10 > 1 || frac % 10 < 0)  //* to check if the number is bin or not
+        int intValue = integer[i] - '0';
+        result += intValue * std::pow(2,j);
+        if(intValue > 1 || intValue < 0)
         {
             throw std::domain_error("not binary system!");
         }
-        frac  = frac / 10;
-        i--;
     }
+
+    return result;
+}
+//*
+float fracBinToDec(const std::string & frac) //* takes the fraction from the bin number
+{
+    int j = -1;
+    float result = 0;
+    for (std::size_t i = 0; i < frac.length(); i++, j--)
+    {
+        int intFrac = frac[i] - '0';
+        result += intFrac * std::pow(2,j);
+        if(intFrac > 1 || intFrac < 0)
+        {
+            throw std::domain_error("not binary system!");
+        }
+    }
+    
     return result;
 }
 int main()
@@ -44,8 +43,14 @@ int main()
     std::string input = "";
     std::getline(std::cin, input);
     std::string sFraction = "";
-    bool check = false;
     //*
+    std::string integer = "";
+    for (char i : input)
+    {
+        if(i == '.') break;
+        integer += i;
+    }
+    bool check = false;
     for (int i = input.length()-1; i>0;i--)
     {
         if (input[i] == '.') 
@@ -58,14 +63,11 @@ int main()
     //*
     if(check == false)
     {
-        unsigned long  rDecimal = std::stoull(input);
-        std::cout<<"- result in Decimal system: ("<<rBinToDec(rDecimal)<<")10"<<std::endl;
+        std::cout<<"- result in Decimal system: ("<<rBinToDec(integer)<<")10"<<std::endl;
     }
     else
     {
-        unsigned long  real = std::floor(std::stoul(input));
-        unsigned long  frac = std::stoul(sFraction);
-        std::cout<<"- the result in Decimal system: ("<<std::setprecision(21)<<rBinToDec(real)+fracBinToDec(frac)<<")10"<<std::endl;
+        std::cout<<"- the result in Decimal system: ("<<std::setprecision(21)<<rBinToDec(integer)+fracBinToDec(sFraction)<<")10"<<std::endl;
     }
     std:: cout<<"----------------------------------------------------------------"<<std:: endl;
     main();
